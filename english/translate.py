6b9926a1bba54ba364319325046b5420e28f2fdf
oybek <aybek.hashimov@gmail.com>
2017-09-22 17:28:30 +0500

    wrote text translation script

--- english/translate.py
+++ english/translate.py
@@ -3,15 +3,33 @@
 import sys
 import json
 import requests
+import re
 
 if len(sys.argv) != 2:
-    sys.exit( "usage: {cmd} \"english text\"".format(cmd=sys.argv[0]) )
+    sys.exit( "usage: {cmd} <file name>".format(cmd=sys.argv[0]) )
 
-url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q={text}"\
-    .format(text=sys.argv[1])
+# read text file
+text_file = open(sys.argv[1], "r")
 
-resp = requests.get(url=url)
-data = json.loads(resp.text)
+file_str = text_file.read()
 
-print(data[0][0][0])
+# split it into sentences
+sentences = re.split('\.|\n\n', file_str)
+#sentences = file_str.split(".")
+
+for sentence in sentences:
+    sentence = sentence.replace("\n", "").strip()
+
+    if len(sentence) == 0:
+        continue
+
+    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q={text}"\
+        .format(text=sentence)
+
+    resp = requests.get(url=url)
+    data = json.loads(resp.text)
+
+    print(sentence)
+    print(data[0][0][0])
+    print('\n')
 
