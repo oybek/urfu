a5747159c0fbf608604a136385788b8dd0192225
unknown <student@server03>
2017-10-31 08:58:44 +0500

    commited visual studio project

---
+++ matrix-calc/MatrixCalc/MatrixCalc/MatrixCalc.cpp
@@ -0,0 +1,13 @@
+// MatrixCalc.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, world!\n" << std::endl;
+
+    return 0;
+}
+
